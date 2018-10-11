bffa329185283897487ef5a54126f595c125f5b0
trent <you@example.com>
2018-10-11 15:15:05 -0600

    Use SHA512 correctly, and use the standard encoding.

--- server/handler/hasher.go
+++ server/handler/hasher.go
@@ -3,19 +3,18 @@ package handler
 import (
 	"crypto/sha512"
 	"encoding/base64"
-	"hash"
 )
 
 type Hasher struct {
-	impl hash.Hash
 }
 
 func NewHasher() *Hasher {
 	h := new(Hasher)
-	h.impl = sha512.New()
 	return h
 }
 
 func (h *Hasher) Hash(s string) string {
-	return base64.RawStdEncoding.EncodeToString(h.impl.Sum([]byte(s)))
+	hasher := sha512.New()
+	hasher.Write([]byte(s))
+	return base64.StdEncoding.EncodeToString(hasher.Sum([]byte(nil)))
 }
--- server/handler/hasher_test.go
+++ server/handler/hasher_test.go
@@ -4,20 +4,25 @@ import (
 	"testing"
 )
 
-func TestGetHash(t *testing.T) {
+func TestGetExampleHash(t *testing.T) {
 	h := NewHasher()
-	str := "idc"
+	str := "angryMonkey"
+	result0 := h.Hash(str)
+	expected := "ZEHhWB65gUlzdVwtDQArEyx+KVLzp/aTaRaPlBzYRIFj6vjFdqEb0Q5B8zVKCZ0vKbZPZklJz0Fd7su2A+gf7Q=="
+	if result0 != expected {
+		t.Errorf("Expected (%s) got (%s)", expected, result0)
+	}
+}
+
+func TestGetSameHash(t *testing.T) {
+	h := NewHasher()
+	str := "angryMonkey"
 	result0 := h.Hash(str)
 	result1 := h.Hash(str)
-	if len(result0) != len(result1) {
+	if result0 != result1 {
 		t.Errorf("I don't know how hashes work")
 	}
 
-	for i := range result0 {
-		if result0[i] != result1[i] {
-			t.Errorf("I'm confused")
-		}
-	}
 }
 
 func TestGetOtherHash(t *testing.T) {
